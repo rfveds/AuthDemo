c6c368c4d98cba9f7aa987cff237528a9cf1c839
Karol Kijowski <kkijowski11@gmail.com>
2022-03-25 13:32:52 +0100

    refactoring

--- index.js
+++ index.js
@@ -45,11 +45,7 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
     const { password, username } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    const user = new User({
-        username: username,
-        password: hash
-    })
+    const user = new User({username, password});
     await user.save();
     req.session.user_id = user._id;
     res.redirect('/');
@@ -61,10 +57,11 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { password, username } = req.body;
-    const user = await User.findOne({ username });
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (validPassword) {
-        req.session.user_id = user._id;
+
+    const foundUser = await User.findAndValidate(username, password);
+
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
         res.redirect('/secret');
     } else {
         res.redirect('/login');
--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -10,5 +12,21 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Password cannot be blank']
     }
 })
+
+
+userSchema.statics.findAndValidate = async function (username, password) {
+    const foundUser = await this.findOne({ username });
+
+    const isValid = await bcrypt.compare(password, foundUser.password);
+
+    return isValid ? foundUser : false;
+}
+
+userSchema.pre('save', async function(next){
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+})
+
 
 module.exports = mongoose.model('User', userSchema);
