6724f50442a83aa9d545d04512b3aaf85ffd77f7
Karol Kijowski <kkijowski11@gmail.com>
2022-03-25 10:59:13 +0100

    register with hashed password

--- index.js
+++ index.js
@@ -3,14 +3,38 @@ const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const session = require('express-session');
+const { redirect } = require('express/lib/response');
 
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/auth-demo');
+}
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.send('this is the homepage');
+})
+
 app.get('/register', (req, res) => {
     res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username: username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/');
 })
 
 app.get('/secret', (req, res) => {
